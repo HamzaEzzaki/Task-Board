802158744d40007949f395a3de9a958f7c27082d
HamzaEzzaki <hamzaezzakiedu@gmail.com>
2024-04-19 21:57:17 +0000

    change logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
     
   <header class="flex items-center justify-between px-4 py-2 bg-white-800 text-black border-b-2 border-gray">
     <div class="flex items-center space-x-2">
-      <img src="../assets/logo.svg" alt="Logo" class="h-9 w-9">
+      <img src="../assets/logo1.svg" alt="Logo" class="h-9 w-9">
       <div class="border-r-2 border-gray h-7"></div> <!-- Small line separator -->
       <span class="text-lg font-semibold">Board</span>
     </div>
